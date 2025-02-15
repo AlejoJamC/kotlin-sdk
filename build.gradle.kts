b30cb2720a8ba2c45f4eb2be7c4ab1a7b7926623
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2025-02-15 23:50:50 +0100

    Data classes and ktor implementations

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4") // CIO engine for HTTP
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     testImplementation(kotlin("test"))
 }
 
